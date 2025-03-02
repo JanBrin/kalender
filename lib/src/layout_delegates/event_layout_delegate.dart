6d8180c8bfff6999475dd1c0bcff92f2b8d42c33
Werner Scholtz <werner.scholtz@kdab.com>
2025-03-02 08:54:43 +0200

    fix: dateTimeRangeOnDate extension and added tests to ensure no regression occurs.

--- lib/src/layout_delegates/event_layout_delegate.dart
+++ lib/src/layout_delegates/event_layout_delegate.dart
@@ -90,7 +90,8 @@ abstract class EventLayoutDelegate extends MultiChildLayoutDelegate {
   /// [heightPerMinute] - The per minute of the current view.
   double calculateHeight(CalendarEvent<dynamic> event) {
     final durationOnDate = event.dateTimeRangeAsUtc.dateTimeRangeOnDate(date)?.duration ?? Duration.zero;
-    return ((durationOnDate.inSeconds / 60) * heightPerMinute);
+    final height = ((durationOnDate.inSeconds / 60) * heightPerMinute);
+    return height;
   }
 
   /// Calculates the distance from the start of the day to the start of the [event].
@@ -183,6 +184,7 @@ class OverlapLayoutDelegate extends EventLayoutDelegate {
 
   @override
   void performLayout(Size size) {
+    print('$date : ${events.length}');
     // Calculate the vertical layout data.
     final verticalLayoutData = calculateVerticalLayoutData();
 
